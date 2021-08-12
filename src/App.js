9ac21d33954e52308ca9db0fd5a97b41fcda3868
iliya <iliya.yurievich@yandex.ru>
2021-08-12 15:55:30 +0300

    delete not important components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import Tech from './tech.js';
-import Header from './header.js';
 
 const Apps = () => {
   return (
     <div>
-      <Header />
       <h1>hello</h1>
-      <Tech />
     </div>    
   )
 }

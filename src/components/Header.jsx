9948da6c17f479fd205f89bc1d332c14862f5d15
iliya <iliya.yurievich@yandex.ru>
2021-08-13 14:25:31 +0300

    add components

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png" alt="image" width="90px"/>
+        </header>  
+    )
+}
+
+export default Header;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <nav className="nav">
+        <div>
+          <a>profile</a>
+        </div>
+        <div>
+          <a>msgs</a>
+        </div>
+        <div>
+          <a>news</a>
+        </div>
+        <div>
+          <a>music</a>
+        </div>
+        <div>
+          <a>sttgs</a>
+        </div>
+      </nav>
+      
+    )
+}
+
+export default Nav;

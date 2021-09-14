3e63c08e0da2904700d76fa3e053cac493f3b9f5
iliya <iliya.yurievich@yandex.ru>
2021-09-14 12:34:50 +0300

    testing the style on the authorization page

--- src/components/Login/login.jsx
+++ src/components/Login/login.jsx
@@ -1,10 +1,25 @@
 import React from "react";
-
+import s from "./login.module.css";
 const Login = (props) => {
   return (
-    <div>
-      <input></input>
-      <input></input>
+    <div className={s.main}>
+      <div className={s.container}>
+        <h1>Please Login</h1>
+        <form>
+          <div className={s.formControll}>
+            <input type="text" required />
+            <label>Email</label>
+          </div>
+          <div className={s.formControll}>
+            <input type="password" required />
+            <label>Password</label>
+          </div>
+          <button className={s.btn}>Login</button>
+          <p className={s.text}>
+            Don't have an account?<a href="#">Register</a>
+          </p>
+        </form>
+      </div>
     </div>
   );
 };
